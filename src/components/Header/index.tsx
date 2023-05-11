7b60d5431e92c0848653e13887cc827147f76d5a
Abhishek Raj <abhishekraj@Abhisheks-Air.bbrouter>
2023-05-11 15:45:35 +0530

    added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,9 +11,96 @@ const Header = () => {
   function handleLink() {
     setShowDropNav(false);
   }
+  {
+    /*} const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);*/
+  }
+
+  // const navbarClasses = `navbar navbar-expand-lg navbar-dark bg-blues fixed-top `;
+
+  // Rest of the component code
 
   return (
     <>
+      {/*} <div className="header-bg position-relative">
+        <nav className={navbarClasses}>
+          <div className="container">
+            <Link className="navbar-brand text-gradient" href="/">
+              Zombiepepe
+            </Link>
+            {!isNavOpen ? (
+              <RxHamburgerMenu className="navs menu-icon" onClick={toggleNav} />
+            ) : (
+              <RxCross2 className="navs menu-icon" onClick={toggleNav} />
+            )}
+
+            <div
+              className={`collapse navbar-collapse ${isNavOpen && "show"}`}
+              id="navbarNav"
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-white active"
+                    aria-current="page"
+                    href="/"
+                  >
+                    HOME
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" href="#about">
+                    ABOUT
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" href="#how-to-buy">
+                    HOW TO BUY
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" href="#tokenomics">
+                    TOKENOMICS
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" href="#roadmap">
+                    ROADMAP
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-white btn btn-outline-light"
+                    href="/"
+                  >
+                    Buy Now
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>*/}
       <div className="header-bg position-relative">
         <div className="navbar navbar-expand-lg bg-body-tertiary text-white">
           <div className="container">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "@/styles/globals.css";
 import React from "react";
 import Sites from "@/components/Site";
